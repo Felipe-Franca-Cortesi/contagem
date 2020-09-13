13bd1c825bf7dc096e1b07bfa330c2b020bd4283
Felipe França <contatofelipefranca12@gmail.com>
2020-09-13 18:57:52 -0300

    Contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace contagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("FOGO!");
+        }
+    }
+}
